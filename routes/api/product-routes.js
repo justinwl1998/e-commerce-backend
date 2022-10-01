08dc3832d47606c0d724be02e2a0e9f3a42af36a
Justin Liu <justinwl2097@gmail.com>
2022-10-01 10:51:35 -0500

    added syncing sequelize to server

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -46,7 +46,6 @@ router.post('/', async (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  console.log('we ballin')
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
